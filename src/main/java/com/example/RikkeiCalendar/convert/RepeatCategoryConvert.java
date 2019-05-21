3eac652d8c2786eae55417663734b0724a235e57
Tu <hoangtunguyen195@gmail.com>
2019-05-21 09:18:03 +0700

    Tao Enum

--- src/main/java/com/example/RikkeiCalendar/convert/RepeatCategoryConvert.java
+++ src/main/java/com/example/RikkeiCalendar/convert/RepeatCategoryConvert.java
@@ -6,6 +6,7 @@ import com.example.RikkeiCalendar.respones.RepeatCategoryResponse;
 public class RepeatCategoryConvert {
     public static RepeatCategoryResponse convert(RepeatCatetoryEntity repeatCatetoryEntity){
         RepeatCategoryResponse response=new RepeatCategoryResponse();
+        response.setId(repeatCatetoryEntity.getId());
         response.setCateName(repeatCatetoryEntity.getCategoryName().name());
         response.setFinishTime(repeatCatetoryEntity.getFinishTimeRepeat());
         return response;
